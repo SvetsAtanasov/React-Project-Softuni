09e3a0a9b67a6153f56cd6b20d594cff4b538581
Svetoslav <33867723+SvetsAtanasov@users.noreply.github.com>
2023-04-29 21:20:15 +0300

    Removed Handlebar views

--- server/src/express-server.js
+++ server/src/express-server.js
@@ -4,28 +4,16 @@ const server = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-const path = require("path");
-
 //Database
 const { initDatabase } = require("./utils/db");
 
 //Middleware
 const cookieParser = require("cookie-parser");
-
 server.use(cors());
 server.use(cookieParser());
 server.use(express.urlencoded({ extended: true }));
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
-
-//set view engine
-const handlebars = require("express-handlebars");
-server.engine("hbs", handlebars.engine({ extname: "hbs" }));
-server.set("view engine", "hbs");
-server.set("views", "./src/views");
-
-//styles
-server.use("/static", express.static(path.join(__dirname, "/public")));
 
 //controllers
 const { homeRouter } = require("./controllers/homeController");
@@ -47,20 +35,15 @@ const { profileController } = require("./controllers/profileController");
 const { authMiddleware, isGuest, isAuth } = require("./services/authServices");
 server.use(authMiddleware);
 server.use("/", homeRouter);
-// server.use("/create", isGuest);
 server.use("/", createRouter);
-// server.use("/register", isAuth);
 server.use("/", registerRouter);
-server.use("/login", isAuth);
 server.use("/", loginRouter);
-server.use("/logout", isGuest);
 server.use("/", logoutRouter);
 server.use("/", catalogRouter);
 server.use("/", detailsPhotoRouter);
 server.use("/", deletePhotoRouter);
 server.use("/", buyPhotoRouter);
 server.use("/", editPhotoRouter);
-server.use("/profile", isGuest);
 server.use("/", profileController);
 server.use("/", likePhotoRouter);
 server.get("/*", (req, res) => {
