a1f32b41309334983e2637099b5cbe3d07ebda84
Svetoslav <33867723+SvetsAtanasov@users.noreply.github.com>
2023-06-03 17:59:01 +0300

    Config changes

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,3 +1,4 @@
 exports.PORT = 7777;
 exports.SECRET = "VeryVerySecret";
-exports.DB_URL = "mongodb://127.0.0.1:27017/petstagram";
+exports.DB_URL =
+  "mongodb+srv://Svets:LyvC6tlPWLmXrfn6@cluster0.rrcjl.mongodb.net/instagram_clone";
